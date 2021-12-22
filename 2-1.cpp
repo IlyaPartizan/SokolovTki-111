817d7c01cfc5a57da23b77ce26baf214d27b06de
IlyaPartizan <91672947+IlyaPartizan@users.noreply.github.com>
2021-12-22 21:26:33 +0300

    Update 2-1.cpp

--- 2-1.cpp
+++ 2-1.cpp
@@ -6,81 +6,120 @@
 using namespace std;
 
 /**
-*\brief Математическая функция, расчитывающая сумму двух чисел
-*\ а - число 1, имеющие тип данных с плавающей точкойдвойной точности
-*\ b - число 2, имеющие тип данных с плавающей точкойдвойной точности
+*\brief Математическая функция, расчитывающая сумму двух чисел.
+*\ param а - число 1, имеющие тип данных с плавающей точкойдвойной точности.
+*\ param b - число 2, имеющие тип данных с плавающей точкойдвойной точности.
 **/
 
-double Summ(double a, double b);
+double Summ(const double a, const double b);
 
 /**
-*\brief Математическая функция, расчитывающая разность двух чисел
-*\ а - число 1, имеющие тип данных с плавающей точкойдвойной точности
-*\ b - число 2, имеющие тип данных с плавающей точкойдвойной точности
+*\brief Математическая функция, расчитывающая разность двух чисел.
+*\ param а - число 1, имеющие тип данных с плавающей точкойдвойной точности.
+*\ param b - число 2, имеющие тип данных с плавающей точкойдвойной точности.
 **/
 
-double Subtr(double a, double b);
+double Difference(const double a, const double b);
 
 /**
-*\brief Математическая функция, расчитывающая произведение двух чисел
-*\ а - число 1, имеющие тип данных с плавающей точкой двойной точности
-*\ b - число 2, имеющие тип данных с плавающей точкой двойной точности
+*\brief Математическая функция, расчитывающая произведение двух чисел.
+*\ param а - число 1, имеющие тип данных с плавающей точкой двойной точности.
+*\ param b - число 2, имеющие тип данных с плавающей точкой двойной точности.
 **/
 
-double Mult(double a, double b);
+double Composition(const double a, const double b);
 
 /**
-*\brief Математическая функция, расчитывающая частное двух чисел
-*\ а - число 1, имеющие тип данных с плавающей точкой двойной точности
-*\ b - число 2, имеющие тип данных с плавающей точкой двойной точности
+*\brief Математическая функция, расчитывающая частное двух чисел.
+*\ param а - число 1, имеющие тип данных с плавающей точкой двойной точности.
+*\ param b - число 2, имеющие тип данных с плавающей точкой двойной точности.
 **/
 
-double Div(double a, double b);
+double Quotient(double a, double b);
 
 /**
-*\точка входа в программу
-*\ return возвращает - в случае успешного выолнения программы
+ * \brief Математические операции.
+ */
+enum class MathOperations
+{
+    /**
+     * \brief Сумма.
+     */
+    Sum,
+
+    /**
+     * \brief Разность.
+     */
+     Difference,
+
+     /**
+      * \brief Произведение.
+      */
+      Composition,
+      /**
+        * \brief Частное.
+        */
+        Quotient
+};
+
+/**
+*\точка входа в программу.
+*\ return возвращает - в случае успешного выолнения программы.
 **/
 
 int main(){
 	setlocale(LC_ALL, "Russian");
-	double a;
-	double b;
-	cout<<"Введите a"<<endl;
-	cin>>a;
-	cout<<"Введите b"<<endl;
-	cin>>b;
-	int i;//переменная для выбора функций программы в консоли
-	cout<<"Введите 1, чтобы сложить чила"<<endl;
-	cout<<"Введите 2, чтобы вычесть"<<endl;
-	cout<<"Введите 3, чтобы умножить"<<endl;
-	cout<<"Введите 4, чтобы разделить"<<endl;
-	cout<<"Любой другой символ для выхода из программы"<<endl;
-	cin>>i;
-	switch(i){
-		case 1:
-			cout<<Summ(a,b);
-		case 2:
-			cout<<Subtr(a,b);
-		case 3:
-			cout<<Mult(a,b);
-		case 4:
-			cout<<Div(a,b);
-		default:
-			break;
-	}
-	
-	
+    cout << "Введите математическую операцию "
+        << "\nСумма - " << static_cast<int>(MathOperations::Sum)
+        << "\nРазность - " << static_cast<int>(MathOperations::Difference)
+        << "\nПроизведение - " << static_cast<int>(MathOperations::Composition)
+        << "\nЧастное - " << static_cast<int>(MathOperations::Quotient);
+
+    int choose;
+    cin >> choose;
+    
+    double a = 0.0;
+    double b = 0.0;
+    double output = 0.0;
+    
+    cout<< "Введите 2 числа ";
+    cin >> a >> b;
+    const auto choice = static_cast<MathOperations>(choose);
+    
+    switch (choice)
+    {
+    case MathOperations::Sum:
+        output = Summ(a,b); 
+        cout << "Сумма = " << output ;
+        break;
+    case MathOperations::Difference:
+        output = Difference(a,b);
+        cout << "Разность = "<<  output;
+        break;
+    case MathOperations::Composition:
+        output = Composition(a,b);
+        cout << "Произведение = "<< output;
+        break;
+    case MathOperations::Quotient:
+        output = Quotient(a,b);
+        cout << "Частное = "<< output;
+        break;
+    }
+    return 0;
 }
-	double Summ(double a, double b){
+
+double Summ(double a, double b){
 		return a+b;
 	}
-	double Subtr(double a, double b){
-		return a-b;
-	}
-	double Mult(double a, double b){
-		return a*b;
-	}
-	double Div(double a, double b){
-		return a/b;
-	}
+	
+double Difference(double a, double b){
+	return a-b;
+}
+
+double Composition(double a, double b){
+	return a*b;
+}
+
+double Quotient(double a, double b){
+	return a/b;
+}
